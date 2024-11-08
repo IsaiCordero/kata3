4ca551ee084a668c876aef2150cb31afd343bb4e
isai <isaicordero03@gmail.com>
2024-11-08 16:35:48 +0000

    kata3 finished

--- src/main/java/software/ulpgc/kata3/architecture/io/MockBarchartLoader.java
+++ src/main/java/software/ulpgc/kata3/architecture/io/MockBarchartLoader.java
@@ -25,21 +25,22 @@ public class MockBarchartLoader implements BarchartLoader {
     }
 
     private Barchart barchart0(List<Title> titles){
+
         Barchart barchart = new Barchart("Year the movies came out", "Years", "Movies");
-        int count = 0;
-        int count2 = 0;
-        int count3 = 0;
+        int year90To00 = 0;
+        int year00To10 = 0;
+        int year10To20 = 0;
         for (Title title : titles) {
             int year = title.getStartYear();
 
-            if (year >= 1890 && year < 1900) count++;
-            barchart.put("1890-1900", count);
+            if (year >= 1890 && year < 1900) year90To00++;
+            barchart.put("1890-1900", year90To00);
 
-            if (year >= 1900 && year < 1910) count2++;
-            barchart.put("1900-1910", count2);
+            if (year >= 1900 && year < 1910) year00To10++;
+            barchart.put("1900-1910", year00To10);
 
-            if (year >= 1910 && year < 1920) count3++;
-            barchart.put("1910-1920", count3);
+            if (year >= 1910 && year < 1920) year10To20++;
+            barchart.put("1910-1920", year10To20);
 
         }
         return barchart;
